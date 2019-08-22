02170653fefbd07c5682c2a87d8dbc816ee2dfa5
Kristin Barr <kristinbarr@gmail.com>
2019-08-22 19:12:48 -0400

    App.js- added update route and update func

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,50 @@
-import React, { useState } from "react";
-import { Route } from "react-router-dom";
-import SavedList from "./Movies/SavedList";
-import MovieList from "./Movies/MovieList";
-import Movie from "./Movies/Movie";
+import React, { useState } from 'react'
+import { Route } from 'react-router-dom'
+import SavedList from './Movies/SavedList'
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
+import UpdateMovieForm from './Movies/UpdateMovieForm'
 
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
+  const [savedList, setSavedList] = useState([])
+  const [movies, setMovies] = useState([])
+  // console.log('app movies', movies)
+  const addToSavedList = (movie) => {
+    setSavedList([...savedList, movie])
+  }
 
-  const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
-  };
+  const updateMovieList = (movie) => {
+    // console.log('updatemovielist', movie)
+    setMovies([...movies, movie])
+    // console.log('updatemovielist new state', movies)
+  }
 
   return (
     <>
       <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
       <Route
-        path="/movies/:id"
-        render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
+        exact
+        path='/'
+        render={(props) => <MovieList {...props} setMovies={setMovies} />}
+      />
+      <Route
+        path='/movies/:id'
+        render={(props) => {
+          return <Movie {...props} addToSavedList={addToSavedList} />
         }}
       />
+      <Route
+        path='/update-movie/:id'
+        render={(props) => (
+          <UpdateMovieForm
+            {...props}
+            movies={movies}
+            updateMovieList={updateMovieList}
+          />
+        )}
+      />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
