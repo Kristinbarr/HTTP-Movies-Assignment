77998e0458e86b025d7016115ed6136cded71126
Kristin Barr <kristinbarr@gmail.com>
2019-08-22 23:53:16 -0400

    added delete button and axios delete method

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -31,6 +31,17 @@ export default class Movie extends React.Component {
     addToSavedList(this.state.movie)
   }
 
+  deleteMovie = (e) => {
+    e.preventDefault()
+    axios
+      .delete(`http://localhost:5000/api/movies/${this.state.movie.id}`)
+      .then((res) => {
+        this.props.removeMovie(res.data)
+        this.props.history.push(`/`)
+      })
+      .catch((err) => console.log('delete request failed', err.response))
+  }
+
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>
@@ -39,13 +50,18 @@ export default class Movie extends React.Component {
       <div className='save-wrapper'>
         <MovieCard movie={this.state.movie} />
         <div className='save-button' onClick={this.saveMovie}>
-          Add to Save List
+          Save to Movies
         </div>
         <div
           className='edit-button'
-          onClick={() => this.props.history.push(`/update-movie/${this.state.movie.id}`)}
+          onClick={() =>
+            this.props.history.push(`/update-movie/${this.state.movie.id}`)
+          }
         >
-          Edit Movie
+          Edit
+        </div>
+        <div className='delete-button' onClick={this.deleteMovie}>
+          Delete
         </div>
       </div>
     )
